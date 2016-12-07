9cedd1b1c7bfb97fa0df91cd4f1fd2643f72df64
Ramadan_Anaconda <rmadanfci20130197@gmail.com>
2016-12-08 01:42:04 +0200

    checking the git branch out

--- app/src/main/java/com/example/anaconda/sunshine/MainActivity.java
+++ app/src/main/java/com/example/anaconda/sunshine/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.anaconda.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("hello SunShine in git branch V01");
     }
 }
